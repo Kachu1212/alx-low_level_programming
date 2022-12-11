2e3e37dd1156e0fa82add9ed17e9d44db5517c1f
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-11 07:33:35 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry no Argument
+*Description - Display the single digits in base 10
+*Return: Return 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+
+}

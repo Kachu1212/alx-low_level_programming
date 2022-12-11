b6d505b8104d0fc7b9f3170e8a6c9335a59b52a2
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-11 03:39:27 -0800

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - Entry no argument
+*Description - Displahy the alphabet in lowercase
+*Return: Return 0
+*/
+
+int main(void)
+{
+	char a;
+	for(a='a'; a<='z'; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+
+}

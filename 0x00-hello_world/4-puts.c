d8541b19a3f9bef522617a524abbca8470b3fb46
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-10 18:10:38 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*Main-Entry
-* Display strings
+*Main- Entry
+*Display strings
 *Return: Always 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }

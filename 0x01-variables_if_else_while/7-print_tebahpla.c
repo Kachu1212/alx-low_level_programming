330318bd9172c84749815139f5180019aba4ad98
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-11 08:14:41 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+
+/**
+*main - Entry no Argument
+*Description - Display the alphabets in reverse order z to a
+*Return: Return 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a == 'z'; a >= 'a'; a--) 
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+
+}

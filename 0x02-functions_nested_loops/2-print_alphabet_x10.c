2a2ab882f11785a34d3a5ad567677e480a5af97f
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-14 12:36:08 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 -prints lowercase alphabets ten times
+*Return: Return 0 when sucessful
+*/
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
+

6cf93a4e1a463711164b8abcbae3226324bcf242
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-14 07:15:07 -0800

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main- Entry main function
+ *print_alphabet- Display the alphabet, in lowercase
+ *Return: Return 0 when successful
+*/
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	for (a == 'a'; a <= 'z'; a++)
+	{
+
+		_putchar(a);
+	}
+	_putchar('\n');
+}

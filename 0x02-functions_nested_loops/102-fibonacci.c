af57ae984db18f4749d83dadf26b48f482efae5b
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-14 19:06:02 -0800

    50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,25 +8,25 @@
 
 int main(void)
 {
-	 int count;
+int count;
 
-	 unsigned long n1 = 0, n2 = 1, sum;
+unsigned long n1 = 0, n2 = 1, sum;
 
-	 for (count = 0; count < 50; count++)
-	 {
+for (count = 0; count < 50; count++)
+{
 
-		 sum = n1 + n2;
-		 printf("%lu", sum);
+sum = n1 + n2;
+printf("%lu", sum);
 
-		 n1 = n2;
-		 n2 = sum;
+n1 = n2;
+n2 = sum;
 
-		 if (count == 49)
-			 printf("\n");
-		 else
-			 printf(", ");
+if (count == 49)
+printf("\n");
+else
+printf(", ");
 
-	 }
+}
 
-	 return (0);
+return (0);
 }

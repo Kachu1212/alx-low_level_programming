55c019f2ba9e496e4aa52edc5fda6b9c0ec9904c
Chukwuemeka Nwosu <cknwosu@gmail.com>
2022-12-19 18:42:02 -0800

    Prints a string, in reverse,

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,24 +2,24 @@
 
 /**
  * print_rev -  print reverse function
- * @str - string characters
+ * @s - string characters
  * Description - prints a string, in reverse, followed by a new line
  *
  * Return: Always 0.
  */
 
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i = 0;
 
 	while (i >= 0)
 	{
-		if (str[i] == '\0')
+		if (s[i] == '\0')
 			break;
 		i++;
 	}
 
 	for (i--; i >= 0; i--)
-		_putchar(str[i]);
+		_putchar(s[i]);
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
+void print_rev(char *s);
 int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
